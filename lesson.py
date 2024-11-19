b8b24b15749cf13bd1a0bd792de8401d5b91469d
Epyur <eugpolishchuk@gmail.com>
2024-11-19 18:14:05 +0300

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,7 @@
+My_string = input('Who are you? ')
+print(len(My_string))
+print(My_string .upper())
+print(My_string .lower())
+print(My_string, ' is sounds like somthing big' .replace(' ', ''))
+print(My_string[0])
+print(My_string[-1])
