fe638bfaba4789ff55704ec0438c60d35a837485
Epyur <eugpolishchuk@gmail.com>
2024-12-01 19:48:12 +0300

    Add files via upload

--- modul_2_5.py
+++ modul_2_5.py
@@ -1,9 +1,10 @@
 def get_matrix(n, m, value):
     matrix = []
     for i in range(n):
-        matrix.append(())
+        matrix1 = []
         for j in range(m):
-           matrix.append(value)
+           matrix1.append(value)
+        matrix.append(matrix1)
     return matrix
 
 matrix1 = get_matrix(2, 2, 10)
